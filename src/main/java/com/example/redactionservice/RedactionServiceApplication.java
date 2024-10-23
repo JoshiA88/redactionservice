d5a04a4d1d46506055c8bfcf853f00e0e8299189
Aniket Joshi <aniketjoshi786@gmail.com>
2024-10-23 12:55:07 +0530

    Second commit

--- src/main/java/com/example/redactionservice/RedactionServiceApplication.java
+++ src/main/java/com/example/redactionservice/RedactionServiceApplication.java
@@ -8,6 +8,7 @@ public class RedactionServiceApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Application starts From Here...");
 		SpringApplication.run(RedactionServiceApplication.class, args);
 
 	}
